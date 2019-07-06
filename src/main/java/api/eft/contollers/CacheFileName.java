51e5a23b0e3ba72ef6981845947734e26d08d9d4
AcSlayter <AcSlayter@users.noreply.github.com>
2019-07-05 18:13:29 -0700

    Update CacheFileName.java

--- src/main/java/api/eft/contollers/CacheFileName.java
+++ src/main/java/api/eft/contollers/CacheFileName.java
@@ -16,10 +16,34 @@ public class CacheFileName  {
         File[] listOfFiles = folder.listFiles();
         for(File file : listOfFiles) {
             String name = file.getName();
-             name = name.replace(".json", "");
+            name = name.replace(".json", "");
+            name = removeChars(name);
             arrayList.add(name);
 
         }
         return arrayList;
+    }
+
+    private String removeChars(String name) {
+          name = name
+                .replaceAll("%20"," ")
+                .replaceAll("%3A",":")
+                .replaceAll("%3B", ";")
+                .replaceAll("%40","@")
+                .replaceAll("%3C","<")
+                .replaceAll("%3E", ">")
+                .replaceAll("%3D","=")
+                .replaceAll("%26", "&")
+                .replaceAll("%24", "\\$")
+                .replaceAll("%23", "#")
+                .replaceAll("%2B", "\\+")
+                .replaceAll("%2C",",")
+                .replaceAll("%3F","\\?")
+                .replaceAll("%93" , "“")
+                .replaceAll("%94","”")
+                .replaceAll("%22","\"")
+                .replaceAll("%2F", "/")
+                .replaceAll("%25","%");
+        return name;
     }
 }
